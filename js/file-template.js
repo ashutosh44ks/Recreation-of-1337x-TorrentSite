4e8452f4df235bbce7a81e66c69b6712bd8418fd
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2020-11-01 15:11:42 +0530

    update all
    hided some features in mobile version and added more dynamic values in file-template

--- js/file-template.js
+++ js/file-template.js
@@ -7,17 +7,12 @@ window.addEventListener('load', (event) => {
 function getMedia(searchID){
   let loader= document.getElementById('loader');
   let resultHead= document.getElementById('result-head');
-  let tDetParent= document.getElementById('Tdetailsparent');
-  let desc1= document.getElementById('description1');
-  let desc2= document.getElementById('description2');
-  let descPic= document.getElementById('description-pic');
   loader.classList.remove("d-none"); //to show loader
   axios.get('https://www.omdbapi.com/?apikey=45ad3714&i='+searchID) //to get results from OMDB API
     .then( (response)=>{
       let media= response.data;
       console.log(media);
 
-      console.log(media.Language);
       if(media.Language.indexOf(",")>=0){
         media.Language=media.Language.substr(0,media.Language.indexOf(','));
       }
@@ -25,20 +20,20 @@ function getMedia(searchID){
       let output=`<div class="row" id="Tdetailsparent">
                     <div class="col-md-4 col-sm-12">
                       <ul class="Tdetails">
-                        <li class="border-bottom border-secondary">Category <span class="float-right text-white-50"> Cate1</span></li>
-                        <li class="border-bottom border-secondary">Type <span class="float-right text-white-50"> ${media.Type}</span></li>
+                        <li class="border-bottom border-secondary">Category <span class="float-right text-white-50"> ${media.Type}</span></li>
+                        <li class="border-bottom border-secondary">Type <span class="float-right text-white-50"> ${media.Rated} </span></li>
                         <li class="border-bottom border-secondary">Language <span class="float-right text-white-50"> ${media.Language}</span></li>
-                        <li class="border-bottom border-secondary">Total size <span class="float-right text-white-50"> xx MB</span></li>
+                        <li class="border-bottom border-secondary">Total size <span class="float-right text-white-50"> ${Math.floor(13*Math.random()+1)+"GB"}</span></li>
                         <li class="border-bottom border-secondary">Uploaded by <span class="float-right text-white-50"> Adam</span></li>
                       </ul>
                     </div>
                     <div class="col-md-4 col-sm-12">
                       <ul class="Tdetails">
-                        <li class="border-bottom border-secondary">Downloads <span class="float-right text-white-50"> dnld1</span></li>
-                        <li class="border-bottom border-secondary">Last checked <span class="float-right text-white-50"> chkd1</span></li>
-                        <li class="border-bottom border-secondary">Last downloaded <span class="float-right text-white-50"> Latdl1</span></li>
-                        <li class="border-bottom border-secondary">Seeders <span class="text-success float-right text-white-50"> seed1</span></li>
-                        <li class="border-bottom border-secondary">Leechers <span class="text-danger float-right text-white-50"> leech1</span></li>
+                        <li class="border-bottom border-secondary">Downloads <span class="float-right text-white-50"> ${Math.floor(1000*Math.random())}</span></li>
+                        <li class="border-bottom border-secondary">Last checked <span class="float-right text-white-50"> Recent </span></li>
+                        <li class="border-bottom border-secondary">Last downloaded <span class="float-right text-white-50"> Recent </span></li>
+                        <li class="border-bottom border-secondary">Seeders <span class="text-success float-right text-success-50"> ${Math.floor(100*Math.random())}</span></li>
+                        <li class="border-bottom border-secondary">Leechers <span class="text-danger float-right text-danger-50"> ${Math.floor(100*Math.random())}</span></li>
                       </ul>
                     </div>
                     <div class="col-md-4 col-sm-12">
@@ -49,10 +44,10 @@ function getMedia(searchID){
                     </div>`;
       loader.classList.add("d-none"); //to hide loader
       resultHead.innerHTML=media.Title;
-      tDetParent.innerHTML=output;
-      desc1.innerHTML=media.Plot;
-      descPic.style.backgroundImage = `url('${media.Poster}')`;
-      desc2.innerHTML=`${media.Title} <br/> ${media.Year} <br/> ${media.Rated} <br/> ${media.Released} <br/> ${media.Genre} <br/> ${media.Language} <br/> ${media.Country} <br/> ${media.Metascore}`;
+      document.getElementById('Tdetailsparent').innerHTML=output;
+      document.getElementById('description1').innerHTML=media.Plot;
+      document.getElementById('description-pic').style.backgroundImage = `url('${media.Poster}')`;
+      document.getElementById('description2').innerHTML=`${media.Title} <br/> ${media.Year} <br/> ${media.Rated} <br/> ${media.Released} <br/> ${media.Genre} <br/> ${media.Language} <br/> ${media.Country} <br/> ${media.Metascore}`;
     })
     .catch( (err)=>{
       loader.classList.add("d-none"); //to hide loader
